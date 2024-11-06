0e317a3315094636f6e902e9fdd0837502c062cc
Consultado-Kirby <kirbyconsultado@gmail.com>
2024-11-06 10:34:01 +0800

    Upload these files.

---
+++ try1.py
@@ -0,0 +1,17 @@
+# Kirby Consultado   DIT 2-1
+import os
+os.system('cls')
+
+print("Enter Admin ID: ")
+admin_id = input(" | ")
+
+admins = ["admin0001", "admin0002", "admin0003"]
+while admin_id in admins:
+    print("ADMIN")
+    print("\nEnter Admin ID: ")
+    admin_id = input(" | ")
+    if admin_id not in admins:
+        print("NOT ADMIN")
+        break
+    
+print("\nProgram end.")
